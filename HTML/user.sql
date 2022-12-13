5660d4a0bf8cde50a0a26a0311637f19ef2c1760
manhmanh2002 <chinhnam1407@gmail.com>
2022-12-13 10:54:39 +0700

    update sql

--- HTML/user.sql
+++ HTML/user.sql
@@ -6,23 +6,17 @@ DROP DATABASE IF EXISTS `login_data`;
 CREATE DATABASE `login_data`;
 
 CREATE TABLE `users` (
-  `id` varchar(10) NOT NULL,
+  `id` int NOT NULL auto_increment,
   `account` varchar(35) DEFAULT NULL,
   `password` varchar(20) DEFAULT NULL,
   `name_users` varchar(30) DEFAULT NULL,
-  `gioitinh` varchar(5) DEFAULT NULL
+  `gioitinh` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
   
 INSERT INTO `users` (`id`, `account`, `password`, `name_users`, `gioitinh`) VALUES
-('ID01', 'account1', '1234', 'Chinh', 'nam'),
-('ID02', 'account2', '1234', 'Duc Anh', 'nam'),
-('ID03', 'account3', '1234', 'Dat', 'nam');
+('1', 'account1', '1234', 'Chinh', 'nam'),
+('2', 'account2', '1234', 'Duc Anh', 'nam'),
+('3', 'account3', '1234', 'Dat', 'nam');
 
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-COMMIT;
-
-///abccccc
-////bvbvbvbvbvbvbvvbvb
-//vnvnvv
