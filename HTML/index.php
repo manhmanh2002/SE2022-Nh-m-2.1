384bc74c15a09443dcee949fe5576e1a13b888f2
RoronoalZoro <90810352+RoronoalZoro@users.noreply.github.com>
2023-01-15 21:59:33 +0700

    Add files via upload

--- HTML/index.php
+++ HTML/index.php
@@ -21,21 +21,23 @@
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
+    <!-- model-viewer -->
+    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script> 
+    <script nomodule src="https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.min.js"></script> 
+
 </head>
 <body>
-    <!-- header section starts  -->
+
+<!-- header section starts  -->
 
 <header class="header">
-
     <a href="#" class="logo"> <i class="fas fa-shopping-basket"></i> model-viewer </a>
-
+    
     <nav class="navbar">
         <a href="#home">home</a>
-        <a href="#features">features</a>
         <a href="#products">products</a>
-        <a href="#categories">categories</a>
+        <a href="#edit"> user edit</a>
         <a href="#review">review</a>
-        <a href="#blogs">blogs</a>
     </nav>
 
     <div class="icons">
@@ -72,6 +74,223 @@
     </form>
 
 </header>
+
+<!-- home section start -->
+<section class="home" id="home">
+    <div class="content">
+        <h3>Augmented Reality and <span>Virtual Reality</span></h3>
+        <p>technology is the closest thing to magic that exists in this world</p>
+    </div>
+</section>
+<!-- home section end -->
+
+<!-- products section starts -->
+<section class="products" id="products">
+
+    <h1 class="heading"> our <span>products</span> </h1>
+
+    <div class="swiper product-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide box">
+                <model-viewer src="assets/login/Drossel.gltf" alt="model robot" auto-rotate camera-controls ar ios-src="assets/login/Drossel.gltf"></model-viewer>
+                <h3>Drossel</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href="#view1" class="btn">view</a>
+            </div>
+
+            <div class="swiper-slide box">
+                <!-- <img src="assets/images/cart-img-2.png" alt=""> -->
+                <model-viewer src="assets/modelviewer/NeilArmstrong.glb" alt="model robot" auto-rotate camera-controls ar ios-src="assets/modelviewer/NeilArmstrong.glb"></model-viewer>
+                <h3>NeilArmstrong</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href="#" class="btn">view</a>
+            </div>
+
+            <div class="swiper-slide box">
+                <!-- <img src="assets/images/cart-img-2.png" alt=""> -->
+                <model-viewer src="assets/modelviewer/RobotExpressive.glb" alt="model robot" auto-rotate camera-controls ar ios-src="assets/modelviewer/RobotExpressive.glb"></model-viewer>
+                <h3>RobotExpressive</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href="#" class="btn">view</a>
+            </div>
+
+            <div class="swiper-slide box">
+                <!-- <img src="assets/images/cart-img-2.png" alt=""> -->
+                <model-viewer src="assets/modelviewer/helicopter_v2.glb" alt="model robot" auto-rotate camera-controls ar ios-src="assets/modelviewer/helicopter_v2.glb"></model-viewer>
+                <h3>Helicopter</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href="#" class="btn">view</a>
+            </div>
+
+            <div class="swiper-slide box">
+                <!-- <img src="assets/images/cart-img-2.png" alt=""> -->
+                <model-viewer src="assets/modelviewer/Helicopter.fbx" alt="model robot" auto-rotate camera-controls ar ios-src="assets/modelviewer/Helicopter.fbx"></model-viewer>
+                <h3>Helicopter</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href="#" class="btn">view</a>
+            </div>
+
+        </div>
+
+        
+
+    </div>
+
+</section>
+<!-- products section end -->
+
+<!-- user edit section start -->
+<section class="edit" id="edit">
+    <h1 class="heading"> user <span>edit</span> </h1>
+</section>
+<!-- user edit section end -->
+
+<!-- review section starts  -->
+
+<section class="review" id="review">
+
+    <h1 class="heading"> customer's <span>review</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="assets/images/traianime.jpg" alt="">
+            <p>Life is a story makes yours the best seller</p>
+            <h3>dong duc anh</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="assets/images/AI1.jpg" alt="">
+            <p></p>
+            <h3>nguyen duc chinh</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="assets/images/AI1.jpg" alt="">
+                <p></p>
+                <h3>lam son dat</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+        </div>
+
+        <div class="box">
+            <img src="assets/images/AI1.jpg" alt="">
+                <p></p>
+                <h3>tran tien hao</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- review section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3> model-viewer <i class="fas fa-shopping-basket"></i> </h3>
+            <p>future technology</p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#" class="links"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+            <a href="#" class="links"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+            <a href="#" class="links"> <i class="fas fa-envelope"></i> t65@hus.edu.vn </a>
+            <a href="#" class="links"> <i class="fas fa-map-marker-alt"></i> nguyenTrai, hanoi, vietnam </a>
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#home" class="links"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="#products" class="links"> <i class="fas fa-arrow-right"></i> products </a>
+            <a href="#edit" class="links"> <i class="fas fa-arrow-right"></i> user edit </a>
+            <a href="#review" class="links"> <i class="fas fa-arrow-right"></i> review </a>
+        </div>
+
+        <div class="box">
+            <h3>newsletter</h3>
+            <p>subscribe for latest updates</p>
+            <input type="email" placeholder="your email" class="email">
+            <input type="submit" value="subscribe" class="btn">
+            <img src="image/payment.png" class="payment-img" alt="">
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span> group SE2022-2.1 </span> | all rights reserved </div>
+
+</section>
+
+<!-- footer section ends -->
 
 <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
 
